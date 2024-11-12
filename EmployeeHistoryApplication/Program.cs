c5c51341a9839deaeea50712d4f39175d6bd0e98
Marija Gjorgjieva <marija.gjorgjieva@edusoft2.com>
2024-11-12 16:02:12 +0100

    Small ui changes

--- EmployeeHistoryApplication/Program.cs
+++ EmployeeHistoryApplication/Program.cs
@@ -2,22 +2,22 @@
 using Microsoft.Extensions.DependencyInjection;
 using EmployeeHistoryApplication.Data;
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<EmployeeHistoryApplicationContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeHistoryApplicationContext") ?? throw new InvalidOperationException("Connection string 'EmployeeHistoryApplicationContext' not found.")));
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//za errors?
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+//od http vo https
 app.UseHttpsRedirection();
 app.UseRouting();
 
@@ -30,5 +30,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-
+//ja run apliakcijata
 app.Run();
--- EmployeeHistoryApplication/obj/Debug/net9.0/EmployeeHistoryApplication.AssemblyInfo.cs
+++ EmployeeHistoryApplication/obj/Debug/net9.0/EmployeeHistoryApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeHistoryApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53079febfbf467b539586623903c2b3c8656d728")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeHistoryApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeHistoryApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
