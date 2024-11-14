f29597fa68fe899cc91fb978e01db0b194d7df28
Marija Gjorgjieva <marija.gjorgjieva@edusoft2.com>
2024-11-14 12:37:55 +0100

    slider added for delete confirmation

--- EmployeeHistoryApplication/obj/Debug/net9.0/EmployeeHistoryApplication.AssemblyInfo.cs
+++ EmployeeHistoryApplication/obj/Debug/net9.0/EmployeeHistoryApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeHistoryApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86712067cf5383f1fc29fe6e29753656f6b5f147")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33ed6436e097e233a5fa4ffc6221ea222b5b6fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeHistoryApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeHistoryApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
