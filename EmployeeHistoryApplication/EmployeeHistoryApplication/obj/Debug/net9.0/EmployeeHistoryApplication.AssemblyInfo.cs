e571e1c8d8d448468c3b423c4b6a492a43eda701
Marija Gjorgjieva <marija.gjorgjieva@edusoft2.com>
2024-11-22 16:54:56 +0100

    telerik finish

--- EmployeeHistoryApplication/EmployeeHistoryApplication/obj/Debug/net9.0/EmployeeHistoryApplication.AssemblyInfo.cs
+++ EmployeeHistoryApplication/EmployeeHistoryApplication/obj/Debug/net9.0/EmployeeHistoryApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeHistoryApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+229c9dbe030218f9d2cf1f18bc28f6f0c6fe9e97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74362ca04f15a76df519111c5ffb483ad1ee5a59")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeHistoryApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeHistoryApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
